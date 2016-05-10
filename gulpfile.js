64caee45be0e3b98b5aa3036d04f0a38039bd299
Yura Griga <grigach@gmail.com>
2016-05-10 18:21:48 +0300

    beta

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,41 @@
 
 const gulp = require('gulp');
 const jasmine = require('gulp-jasmine');
+const del = require('del');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
-gulp.task('test', function(){
+gulp.task('clean', function () {
+    return del(['dist'])
+});
+
+gulp.task('compress', function () {
+    return gulp.src('src/main.js')
+        .pipe(uglify())
+        .pipe(rename('showdown-classify.min.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('dist', function () {
+    return gulp.src('src/main.js')
+        .pipe(rename('showdown-classify.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('test', function () {
     gulp.src('spec/test.js').pipe(jasmine()).on('error', swallowError)
 });
 
 
-gulp.task('watch', function(){
+gulp.task('watch', function () {
     return gulp.watch([
         'src/**/*.*',
         'spec/**/*.*'
-    ],['test'])
+    ], ['test'])
 });
 
-gulp.task('default',['test', 'watch']);
+gulp.task('default', ['test', 'watch']);
+gulp.task('build', ['clean', 'dist', 'compress']);
 
 
 var swallowError = function (error) {
