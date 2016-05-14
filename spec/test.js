94072a2dee469e9a60a61fdcbba8014a8556df12
Yura Griga <grigach@gmail.com>
2016-05-14 13:04:20 +0300

    more tests

--- spec/test.js
+++ spec/test.js
@@ -13,4 +13,15 @@ describe("Showdown class sugar extension spec", function () {
         let expectation = html === '<p><img class="thumbnail" src="http://lorempixel.com/400/200" alt="image alt" ></p>'
         expect(expectation).toBe(true);
     });
+
+    it("add desired class to image with no alt", function () {
+        let converter = new showdown.Converter({ extensions: ['classify'] });
+        let text = '![](http://lorempixel.com/400/200){.thumbnail}';
+        let html = converter.makeHtml(text);
+
+        let expectation = html === '<p><img class="thumbnail" src="http://lorempixel.com/400/200" alt="" ></p>'
+        expect(expectation).toBe(true);
+    });
+
+
 });
